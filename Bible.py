55f82e55d567a64318cdda618b06c9a99a35cd16
Robert Westbrook <robert_westbrook@alumni.baylor.edu>
2020-05-30 14:04:10 -0400

    A lot of updates to original design

--- Bible.py
+++ Bible.py
@@ -19,15 +19,61 @@ def chapterSearch(name, chapter):
     chapterOfChoice = chaptersToChooseFrom[chapterChoice]
     verseElement = [v for v in chapterOfChoice.findall('v')]
     print ("\n-~|" + bookOfChoice.get('n') + " " + chapterOfChoice.get('n') + "|~-\n")
+    textList = []
     for v in verseElement:
-        text = v.get('n') + "| " + v.text
-        print (text)
+        text = v.get('n') + "| " + v.text +"\n"
+        textList.append(text)
+    return textList
+
+
+def chapterQuantity(name):
+    '''Get the chapters that are availble in a user selected book'''
+    chNum = len(book[name].findall('c'))
+    return chNum
+
+
+def verseQuantity():
+    '''Get the number of verses to pick from in user selected chapter.'''
+    vNum = len(chapterSearch(name, chapter))
+    return vNum
+
+
+def textFormat():
+    textList = chapterSearch(name, chapter)
+    for t in textList:
+        formattedText = print (t)
+    return formattedText
+
+
+def getBook():
+    for t in title:
+        bookList = print (t)
+    name = str(input(f"Here is a list of all of the books: {bookList}\nWhat Book Would you like to Read? >> "))
+    if name in book:
+        return name
+    else:
+        print ("Invalid book")
+        getBook()
+
+
+def getChapter(name): #still need to work out bugs on validation
+    num = chapterQuantity(name)
+    chapter = input(f"There are {num} chapters in that book...\n"
+                "What chapter would you like to read? >> ")
+    return chapter
+
+
+# def getVerse(name, chapter):
+#     fullChapter = chapterSearch(name, chapter)
+#     verseInput = print(input(f'There are {verseQuantity()} verses in {name} {chapter}'))
+#     pass
 
 
 # ___________________USER INPUT____________________#
 # Asks for user input to search a particular chapter of the bible
-name = str(input("What Book >> ")) 
-chapter = str(input("What chapter >> "))
+name = getBook()
+chapter = getChapter(name)
+
 
 #  Testing Purpose!!!
 # name = "John"
@@ -35,7 +81,13 @@ chapter = str(input("What chapter >> "))
 
 
 #__________________EXECUTION:_________________#
-chapterSearch(name, chapter)
+# getVerse(name, chapter)
+# print(chapterSearch(name, chapter))
+# getVerse()
+# verseQuantity()
+# chapterQuantity(name)
+textFormat()
+
 
 # ___________________TO DO:_______________________#
 # - Create function that checks to see if user input is accurate.
@@ -43,5 +95,5 @@ chapterSearch(name, chapter)
 # - Create a way to search specific verse via address book - chapter: verse/
 #   sections of verses.
 #   Create a way to log what user has read and save progress.
-# - Create an Optional GUI that allows 
+# - Create an Optional GUI that allows.
 
